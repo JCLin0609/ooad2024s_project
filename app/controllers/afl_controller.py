a9da92f99f6425581f096bbde6ec57f9142645ce
JCLin0609 <as11675847@gmail.com>
2024-04-13 01:41:17 +0800

    Modify controller and template.

--- app/controllers/afl_controller.py
+++ app/controllers/afl_controller.py
@@ -3,8 +3,14 @@ from flask import Blueprint
 from app.services import afl_service
 
 bp = Blueprint('afl_controller',__name__)
-
+    
 @bp.route('/')
 def index():
-    users = afl_service.users()
+    users = aflService.users()
     return render_template('index.html', users=users)
+
+@bp.route('/upload')
+def upload():
+    return render_template('UploadPage.html')
+
+aflService = afl_service.FuzzService()
