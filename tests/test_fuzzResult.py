989c7b6a8b95cf534156bbe06cde95a0c1548836
Jack <ch097711@gmail.com>
2024-05-01 15:01:50 +0800

    Add unit tests for FuzzResult model

--- tests/test_fuzzResult.py
+++ tests/test_fuzzResult.py
@@ -1,5 +1,7 @@
 from app.models.fuzzResult import FuzzResult, Crash, FuzzData
+from unittest.mock import Mock, patch
 import pytest
+
 
 @pytest.fixture
 def fuzz_result():
@@ -13,11 +15,25 @@ def fuzz_result():
     ]
     return FuzzResult(crashes, fuzz_data)
 
+
 def test_num_crashes(fuzz_result):
     assert fuzz_result.num_crashes == 2
+
 
 def test_get_crashed(fuzz_result):
     assert len(fuzz_result.get_crashed) == 2
 
+
 def test_num_fuzz_data(fuzz_result):
     assert fuzz_result.num_fuzz_data == 2
+
+
+def test_gen_target_report(fuzz_result):
+    # Arrange
+    with patch("app.models.fuzzResult.afl_command_helper.plot_fuzz_imgs") as mock:
+
+        # Act
+        fuzz_result.gen_target_report("test")
+
+        # Assert
+        mock.assert_called_once_with("test")
