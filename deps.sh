06c474f39889fd4a2bd42a337e1979e0dfdca978
Jack <ch097711@gmail.com>
2024-06-10 23:26:31 +0800

    add the install AFLplusplus script to deps.sh

--- deps.sh
+++ deps.sh
@@ -5,4 +5,9 @@ mkdir ttyd
 wget -O ttyd/ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.x86_64
 chmod +x ttyd/ttyd
 
-#install afl
+#install afl++
+rm -rf ./AFLplusplus
+git clone https://github.com/AFLplusplus/AFLplusplus
+cd ./AFLplusplus
+make distrib
+sudo make install
